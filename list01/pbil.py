a13700c4b2cb4a268415acf427179e6a4501146f
i279079 <i279079@lab110-10.stud.ii>
2019-03-16 22:09:51 +0100

    troce lepiej

--- list01/pbil.py
+++ list01/pbil.py
@@ -1,45 +1,45 @@
 import numpy as np
+import matplotlib as plt
 
-def pbil(
-    f,
-    vec_size=10,
-    prop_mut=0.1,
-    pop_size=100,
-    iterations=100,
-    theta1=0.1,
-    theta2=0.05,
-    theta3=0,
-    **kwargs
-):
-    propability_vector = np.ones(vec_size) * 0.5
-    best_scores_history = []
-    propabilities_vector_history = []
-    for i in range(iterations):
-        new_population = (np.random.rand(pop_size, vec_size) < propability_vector).astype(np.int64)
-
-        pop_score = f(new_population)
-        best = new_population[np.argmax(pop_score)]
-    #         history 
-        best_scores_history.append(pop_score.max())
-        propabilities_vector_history.append(propability_vector)
-    #     Best update
-        propability_vector = np.clip(
-            propability_vector * (1 - theta1) + best * theta1,
-            0,
-            1
-        )
-    # mutation
-        mutated_ids = np.random.rand(vec_size) < theta2
-        propability_vector[mutated_ids] = np.clip(
-            (
-                propability_vector[mutated_ids] * (1 - theta3) +
-                (np.random.rand(mutated_ids.sum()) < 0.5) * theta3
-            ),
-            0,
-            1
-        )
+def pbil(F, d, N, th1, th2, th3, steps=500):  
     
+    plot_probs = np.zeros((steps, d))
+    plot_scores = np.zeros(steps)
     
-    return f(best[np.newaxis, :])[0], best_scores_history,propabilities_vector_history, best, propability_vector
-
-
+    p = np.ones((d, 1)) / 2
+    population = np.random.rand(d, N) < p
+    result = F(population)
+      
+    plot_probs[0] = p.reshape(-1)
+    plot_scores[0] = np.max(result)
+    
+    for i in range(1, steps):
+        
+        best = population[:, [np.argmax(result)]]
+        p = p * (1 - th1) + best * th1
+        
+        if_change = np.random.rand(*(d,1)) < th2
+        p = p * (1 - if_change) + (
+            (if_change) * (
+                p * (1 - th3)
+                + (np.random.rand(*(d,1)) < 0.5) * th3
+            )
+        )
+        p = p.clip(0,1)
+        population = np.random.rand(d, N) < p
+        result = F(population)
+        
+        plot_probs[i] = p.reshape(-1)
+        plot_scores[i] = np.max(result)
+    
+    return plot_scores, plot_probs
+        
+def plot_results(scores, probs, title=""):
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+    fig.suptitle(title)
+    for i in range(scores.shape[1]):
+        axs[0].plot(range(scores.shape[0]), scores[:, i])
+    
+    for i in range(probs.shape[1]):
+        axs[1].plot(range(probs.shape[0]), probs[:,i,0])
+    plt.show()
